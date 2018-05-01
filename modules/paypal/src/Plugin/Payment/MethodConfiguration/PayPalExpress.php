f6a850fd9e0ba7840ee41d793154c3890c111812
Andrew Crook <andrew.crook@somersoft.co.uk>
2018-05-01 15:32:15 +0100

    Add missing use statements.

--- modules/paypal/src/Plugin/Payment/MethodConfiguration/PayPalExpress.php
+++ modules/paypal/src/Plugin/Payment/MethodConfiguration/PayPalExpress.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\omnipay_paypal\Plugin\Payment\MethodConfiguration;
 
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Form\FormStateInterface;
+
 /**
  * Provides the configuration for the PayPal Express payment method plugin.
  *
